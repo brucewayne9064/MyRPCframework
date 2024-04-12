e4a4f4d6bd7f262e45f88a849138e675b830687f
brucewayne9064 <906491934@qq.com>
2024-04-13 02:15:36 +0800

    初始化

---
+++ src/main/java/org/example/myrpcframework/MyRpCframeworkApplication.java
@@ -0,0 +1,13 @@
+package org.example.myrpcframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRpCframeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyRpCframeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/myrpcframework/MyRpCframeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.myrpcframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRpCframeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
