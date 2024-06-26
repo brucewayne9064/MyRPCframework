cf8a4bd8368ea13bf6e29fee46cd401666acfa38
brucewayne9064 <906491934@qq.com>
2024-06-26 22:23:04 +0800

    SocketRpcRequestHandlerRunnable

--- src/main/java/org/example/myrpcframework/rpcFrameworkSimple/remoting/handler/RpcRequestHandler.java
+++ src/main/java/org/example/myrpcframework/rpcFrameworkSimple/remoting/handler/RpcRequestHandler.java
@@ -1,10 +1,14 @@
 package org.example.myrpcframework.rpcFrameworkSimple.remoting.handler;
 
 import lombok.extern.slf4j.Slf4j;
+import org.example.myrpcframework.rpcFrameworkCommon.exception.RpcException;
 import org.example.myrpcframework.rpcFrameworkCommon.factory.SingletonFactory;
 import org.example.myrpcframework.rpcFrameworkSimple.provider.Impl.ZkServiceProviderImpl;
 import org.example.myrpcframework.rpcFrameworkSimple.provider.ServiceProvider;
 import org.example.myrpcframework.rpcFrameworkSimple.remoting.dto.RpcRequest;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 
 @Slf4j
@@ -16,17 +20,23 @@ public class RpcRequestHandler {
         this.serviceProvider = SingletonFactory.getInstance(ZkServiceProviderImpl.class);
     }
 
+    //handle 方法是处理RPC请求的入口。它首先通过 serviceProvider 获取服务实例，
+    //然后调用 invokeTargetMethod 方法来执行具体的远程方法调用。
     public Object handle(RpcRequest rpcRequest){
         Object service = serviceProvider.getService(rpcRequest.getRpcServiceName());
         return invokeTargetMethod(rpcRequest, service);
     }
 
+    //invokeTargetMethod 方法使用Java反射API来动态调用服务对象的方法。
+    //它首先获取方法对象，然后使用该方法的 invoke 方法执行调用，并将结果返回。
     private Object invokeTargetMethod(RpcRequest rpcRequest, Object service){
         Object result;
         try{
-
-        }catch(){
-
+            Method method = service.getClass().getMethod(rpcRequest.getMethodName(), rpcRequest.getParamTypes());
+            result = method.invoke(service, rpcRequest.getParameters());
+            log.info("service:[{}] successful invoke method:[{}]", rpcRequest.getInterfaceName(), rpcRequest.getMethodName());
+        }catch(NoSuchMethodException | IllegalArgumentException | InvocationTargetException | IllegalAccessException e){
+            throw new RpcException(e.getMessage(), e);
         }
 
         return result;
--- src/main/java/org/example/myrpcframework/rpcFrameworkSimple/remoting/transport/socket/SocketRpcRequestHandlerRunnable.java
+++ src/main/java/org/example/myrpcframework/rpcFrameworkSimple/remoting/transport/socket/SocketRpcRequestHandlerRunnable.java
@@ -1,17 +1,37 @@
 package org.example.myrpcframework.rpcFrameworkSimple.remoting.transport.socket;
 
 import lombok.extern.slf4j.Slf4j;
+import org.example.myrpcframework.rpcFrameworkCommon.factory.SingletonFactory;
+import org.example.myrpcframework.rpcFrameworkSimple.remoting.dto.RpcRequest;
+import org.example.myrpcframework.rpcFrameworkSimple.remoting.dto.RpcResponse;
 import org.example.myrpcframework.rpcFrameworkSimple.remoting.handler.RpcRequestHandler;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 @Slf4j
 public class SocketRpcRequestHandlerRunnable implements Runnable{
     private final Socket socket;
-    private final RpcRequestHandler;
+    private final RpcRequestHandler rpcRequestHandler;
+
+    public SocketRpcRequestHandlerRunnable(Socket socket){
+        this.socket = socket;
+        this.rpcRequestHandler = SingletonFactory.getInstance(RpcRequestHandler.class);
+    }
 
     @Override
     public void run() {
-
+        log.info("server handle message from client by thread: [{}]", Thread.currentThread().getName());
+        try(ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream())){
+            RpcRequest rpcRequest = (RpcRequest) objectInputStream.readObject();
+            Object result = rpcRequestHandler.handle(rpcRequest);
+            objectOutputStream.writeObject(RpcResponse.success(result, rpcRequest.getRequestId()));
+            objectOutputStream.flush();
+        }catch(IOException | ClassNotFoundException e){
+            log.error("occur exception:", e);
+        }
     }
 }
